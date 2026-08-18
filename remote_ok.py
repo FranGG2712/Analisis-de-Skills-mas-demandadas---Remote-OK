0752a1b573e64da3bdb4d1f0946215f7ae901803
FranGG2712 <156584750+FranGG2712@users.noreply.github.com>
2026-08-18 16:10:48 -0300

    Refactor remote_ok.py to clean up code

--- remote_ok.py
+++ remote_ok.py
@@ -1,4 +1,3 @@
-# %%
 import time
 import requests
 import pandas as pd
@@ -49,30 +48,5 @@ if datos:
     tabla.to_csv('remote_ok.csv',index=False,encoding='utf-8-sig')
 else:
     print('No se extrajeron datos')
-
-# %%
-import pandas as pd
-import matplotlib.pyplot as plt
-import ast
-
-read = pd.read_csv('remote_ok.csv')
-read['Tag'] = read['Tag'].apply(ast.literal_eval)
-top_skills = read['Tag'].explode().value_counts(ascending=True).tail(15)
-
-#Crear gráfico
-top_skills.plot(kind='barh')
-plt.grid(axis='x',color = 'black', linestyle = '--', linewidth = 1)
-
-plt.title('Skills más demandadas\nRemote OK',loc='left')
-plt.ylabel("Skill")
-
-#Guardar como PNG
-plt.savefig('remoteok_analisis.png')
-
-#Esta línea es opcional si estas en un archivo ipynb (notebook)
-plt.show()
-
-# %%
-
 
 
